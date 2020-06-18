53e7e0adeae9122454c4ccef0f92507a00ef926f
Meet Modi <meetmodi8@gmail.com>
2020-06-19 00:29:46 +0530

    added pdf

--- api/pdf_api/generate_report.php
+++ api/pdf_api/generate_report.php
@@ -1,23 +1,105 @@
 <?php
-    // required headers
-    header("Access-Control-Allow-Origin: http://localhost/SWS_WebApp/");
-    header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: POST");
-    header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    
-    // database connection will be here
-    // files needed to connect to database
-    include_once '../config/database.php';
-    include_once '../objects/complaint.php';
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/SWS_WebApp/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// database connection will be here
+// files needed to connect to database
+include_once('fpdf182/fpdf.php');
 
-    // get database connection
-    $database = new Database();
-    $db = $database->getConnection();
+//$json = file_get_contents("php://input");
+//$data = json_decode($json);
+$data = $_GET['hi'];
+$pdf = new FPDF('L','mm','A4');
+$pdf->SetFont('Times','',11);
+$pdf->AddPage();
+//$pdf->Image('img/logo.jpeg',10,10,10,10,'JPEG');
+$pdf->Image('img/header.jpg',34,10,80,20,'JPG');
+//$pdf->Image('img/logo.jpeg',10,10,10,10,'JPEG');
+$pdf->Image('img/header.jpg',182,10,80,20,'JPG');
+$pdf->SetXY(20,40);
+//$pdf->Cell(125,25,"Complaint ID : \n",1);
+$pdf->Multicell(120,3,"\nComplaint ID: ".$data."                                             Date: 18-06-2020\n",1,'L'); 
+$pdf->SetXY(20,46);
+$pdf->Multicell(120,3,"\nAMC From: 18-06-2020                   AMC TO: 18-06-2020\n",1,'L'); 
+$pdf->SetXY(20,56);
+$pdf->Multicell(120,4,"Name of Customer: SBI     \nContact Person: Jinesh Patel",1,'L'); 
+$pdf->SetXY(20,66);
+$pdf->Multicell(120,4,"Address: opposite CTM CROSS ROADS,\n Ahmedabad-38001",1,'L'); 
+$pdf->SetXY(20,76);
+$pdf->Multicell(120,4,"Mobile : 987654321                       Telephone : 079-12345678\n",1,'L'); 
+$pdf->SetXY(20,82);
+$pdf->Multicell(120,4,"Nature of Complaint : complaint type ",1,'L'); 
+$pdf->SetXY(20,88);
+$pdf->Multicell(120,4,"Remarks : remarks",1,'L'); 
+$pdf->SetXY(20,94);
+$pdf->Multicell(120,4,"Call Assigned to : ",1,'L'); 
+$pdf->SetXY(20,100);
+$pdf->Multicell(120,4,"Defect Observed : ",1,'L'); 
+$pdf->SetXY(20,106);
+$pdf->Multicell(120,4,"Action Taken : ",1,'L'); 
+$pdf->SetXY(20,112);
+$pdf->Multicell(120,4,"Parts Replaced : ",1,'L'); 
+$pdf->SetXY(20,118);
+$pdf->Multicell(120,4,"Remarks : ",1,'L'); 
+$pdf->SetXY(20,124);
+$pdf->Cell(60,4,"Line Voltages : ",1); 
+//$pdf->SetXY(80,116);
+$pdf->Cell(60,4,"Grill Temp : ",1); 
+$pdf->SetXY(20,130);
+$pdf->Cell(60,4,"Current : ",1); 
+//$pdf->SetXY(80,116);
+$pdf->Cell(60,4,"Room Temp : ",1); 
+$pdf->SetXY(20,136);
+$pdf->Multicell(120,4,"Time Worked : From                   to                   Date : ",1,'L'); 
+$pdf->SetXY(20,142);
+$pdf->Multicell(120,4,"Mechanic Remarks : ",1,'L'); 
+$pdf->SetXY(20,148);
+$pdf->Multicell(120,4,"Name & sign of Mechanic : ",1,'L'); 
 
-    $json = file_get_contents("php://input");
-    $data = json_decode($json);
-    
-    
+$pdf->SetXY(158,40);
+//$pdf->Cell(125,25,"Complaint ID : \n",1);
+$pdf->Multicell(120,3,"\nComplaint ID:                                              Date: 18-06-2020\n",1,'L'); 
+$pdf->SetXY(158,46);
+$pdf->Multicell(120,3,"\nAMC From: 18-06-2020                   AMC TO: 18-06-2020\n",1,'L'); 
+$pdf->SetXY(158,56);
+$pdf->Multicell(120,4,"Name of Customer: SBI     \nContact Person: Jinesh Patel",1,'L'); 
+$pdf->SetXY(158,66);
+$pdf->Multicell(120,4,"Address: opposite CTM CROSS ROADS,\n Ahmedabad-38001",1,'L'); 
+$pdf->SetXY(158,76);
+$pdf->Multicell(120,4,"Mobile : 987654321                       Telephone : 079-12345678\n",1,'L'); 
+$pdf->SetXY(158,82);
+$pdf->Multicell(120,4,"Nature of Complaint : complaint type ",1,'L'); 
+$pdf->SetXY(158,88);
+$pdf->Multicell(120,4,"Remarks : remarks",1,'L'); 
+$pdf->SetXY(158,94);
+$pdf->Multicell(120,4,"Call Assigned to : ",1,'L'); 
+$pdf->SetXY(158,100);
+$pdf->Multicell(120,4,"Defect Observed : ",1,'L'); 
+$pdf->SetXY(158,106);
+$pdf->Multicell(120,4,"Action Taken : ",1,'L'); 
+$pdf->SetXY(158,112);
+$pdf->Multicell(120,4,"Parts Replaced : ",1,'L'); 
+$pdf->SetXY(158,118);
+$pdf->Multicell(120,4,"Remarks : ",1,'L'); 
+$pdf->SetXY(158,124);
+$pdf->Cell(60,4,"Line Voltages : ",1); 
+//$pdf->SetXY(80,116);
+$pdf->Cell(60,4,"Grill Temp : ",1); 
+$pdf->SetXY(158,130);
+$pdf->Cell(60,4,"Current : ",1); 
+//$pdf->SetXY(80,116);
+$pdf->Cell(60,4,"Room Temp : ",1); 
+$pdf->SetXY(158,136);
+$pdf->Multicell(120,4,"Time Worked : From                   to                   Date : ",1,'L'); 
+$pdf->SetXY(158,142);
+$pdf->Multicell(120,4,"Mechanic Remarks : ",1,'L'); 
+$pdf->SetXY(158,148);
+$pdf->Multicell(120,4,"Name & sign of Mechanic : ",1,'L'); 
+$pdf->Output();
 
+$pdf->Output();
 ?>
