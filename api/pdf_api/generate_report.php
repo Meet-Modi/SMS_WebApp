ab9afdd3deb66433c6e8b20654a8bfc76dfc1623
Meet Modi <meetmodi8@gmail.com>
2020-06-19 16:06:12 +0530

    print connected

--- api/pdf_api/generate_report.php
+++ api/pdf_api/generate_report.php
@@ -5,58 +5,84 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- 
-// database connection will be here
-// files needed to connect to database
-include_once('fpdf182/fpdf.php');
 
-//$json = file_get_contents("php://input");
-//$data = json_decode($json);
-$data = $_GET['hi'];
+include_once 'fpdf182/fpdf.php';
+include_once '../config/database.php';
+include_once '../objects/customer.php';
+include_once '../objects/amc.php';
+include_once '../objects/complaint_report.php';
+include_once '../objects/complaint_type.php';
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+$complaint = new ComplaintReport($db);
+$customer = new Customer($db);
+
+$json = file_get_contents("php://input");
+$data = json_decode($json);
+
+$complaint->complaint_id=$_POST['complaint_id'];
+$complaint->customer_id = Customer::getCustomerIdFromBillingName($_POST['billing_name'],$db);
+$customer->customer_id = $complaint->complaint_id;
+
+$json_customer=$customer->showCustomer();
+$data_customer = json_decode($json_customer);
+
+$complaint->amc_id = $_POST['amc_id'];
+$json_amc = AMC::getAmcById($_POST['amc_id'],$db);
+$data_amc = json_decode($json_amc);
+
 $pdf = new FPDF('L','mm','A4');
 $pdf->SetFont('Times','',11);
 $pdf->AddPage();
-//$pdf->Image('img/logo.jpeg',10,10,10,10,'JPEG');
+$pdf->Image('img/logo.jpeg',10,10,10,10,'JPEG');
 $pdf->Image('img/header.jpg',34,10,80,20,'JPG');
-//$pdf->Image('img/logo.jpeg',10,10,10,10,'JPEG');
+$pdf->Image('img/logo.jpeg',158,10,10,10,'JPEG');
 $pdf->Image('img/header.jpg',182,10,80,20,'JPG');
 $pdf->SetXY(20,40);
-//$pdf->Cell(125,25,"Complaint ID : \n",1);
-$pdf->Multicell(120,3,"\nComplaint ID: ".$data."                                             Date: 18-06-2020\n",1,'L'); 
+$pdf->Multicell(120,3,"\nComplaint ID: ".$_POST['complaint_id']."                                             Date: ".$_POST['date']."\n",1,'L'); 
 $pdf->SetXY(20,46);
-$pdf->Multicell(120,3,"\nAMC From: 18-06-2020                   AMC TO: 18-06-2020\n",1,'L'); 
+$pdf->Multicell(120,3,"\nAMC From : ".$data_amc->fromdate."                   AMC TO: \n",1,'L'); 
 $pdf->SetXY(20,52);
-$pdf->Multicell(120,4,"\nName of Customer: SBI     \nContact Person: Jinesh Patel\n",1,'L'); 
+$pdf->Multicell(120,4,"\nName of Customer: ".$data_customer->billingname."     \nContact Person: ".$data_customer->firstname." ".$data_customer->lastname."\n",1,'L'); 
 $pdf->SetXY(20,64);
-$pdf->Multicell(120,4,"\nAddress: opposite CTM CROSS ROADS,\n Ahmedabad-38001\n",1,'L'); 
+$pdf->Multicell(120,4,"\nAddress: " . $data_customer->address . " ",1,'L'); 
 $pdf->SetXY(20,76);
-$pdf->Multicell(120,4,"Mobile : 987654321                       Telephone : 079-12345678\n",1,'L'); 
+$pdf->Multicell(120,4,"Mobile : ".$data_customer->contactno1."                      Telephone : 079-12345678\n",1,'L'); 
 $pdf->SetXY(20,80);
-$pdf->Multicell(120,4,"\nNature of Complaint : complaint type\n",1,'L'); 
+$pdf->Multicell(120,4,"\nNature of Complaint : ".$_POST['complaint_type']."\n",1,'L'); 
 $pdf->SetXY(20,88);
-$pdf->Multicell(120,4,"\nRemarks : remarks\n",1,'L'); 
+
+
+$pdf->Multicell(120,4,"\nRemarks : ".$_POST['remarks']."\n",1,'L'); 
 $pdf->SetXY(20,96);
-$pdf->Multicell(120,4,"\nCall Assigned to : \n",1,'L'); 
+$pdf->Multicell(120,4,"\nCall Assigned to : ".$_POST['mechanic_name']."\n",1,'L'); 
 $pdf->SetXY(20,104);
-$pdf->Multicell(120,4,"\nDefect Observed : \n",1,'L'); 
+$pdf->Multicell(120,4,"\nDefect Observed : ".$_POST['defect_observed']."\n",1,'L'); 
 $pdf->SetXY(20,112);
-$pdf->Multicell(120,4,"\nAction Taken : \n",1,'L'); 
+$pdf->Multicell(120,4,"\nAction Taken : ".$_POST['action_taken']."\n",1,'L'); 
 $pdf->SetXY(20,120);
-$pdf->Multicell(120,4,"\nParts Replaced : \n",1,'L'); 
+$pdf->Multicell(120,4,"\nParts Replaced : ".$_POST['parts_replaced']."\n",1,'L'); 
 $pdf->SetXY(20,128);
 $pdf->Multicell(120,4,"\nRemarks : \n",1,'L'); 
 $pdf->SetXY(20,136);
-$pdf->Cell(60,4,"\nLine Voltages : \n",1); 
-$pdf->Cell(60,4,"\nGrill Temp : \n",1); 
+$pdf->Cell(60,4,"\nLine Voltages : ".$_POST['line_voltage']."\n",1); 
+$pdf->Cell(60,4,"\nGrill Temp : ".$_POST['grill_temp']."\n",1); 
 $pdf->SetXY(20,140);
-$pdf->Cell(60,4,"\nCurrent : \n",1); 
-$pdf->Cell(60,4,"\nRoom Temp : \n",1); 
+$pdf->Cell(60,4,"\nCurrent : ".$_POST['current']."\n",1); 
+$pdf->Cell(60,4,"\nRoom Temp : ".$_POST['room_temp']."\n",1); 
 $pdf->SetXY(20,144);
-$pdf->Multicell(120,4,"\nTime Worked : From                   to                   Date : \n",1,'L'); 
+$pdf->Multicell(120,4,"\nTime Worked : From ".$_POST['time_from']."to                   Date : ".$_POST['time_to']."\n",1,'L'); 
 $pdf->SetXY(20,152);
-$pdf->Multicell(120,4,"\nMechanic Remarks : \n",1,'L'); 
+$pdf->Multicell(120,4,"\nMechanic Remarks : ".$_POST['mechanic_remarks']."\n",1,'L'); 
 $pdf->SetXY(20,160);
 $pdf->Multicell(120,4,"\nName & sign of Mechanic : \n\n",1,'L'); 
+$pdf->SetXY(20,168);
+$pdf->Multicell(120,4,"\nCustomer's Remarks : ".$_POST['customer_remarks']."\n\n",1,'L'); 
+
+
 
 $pdf->SetXY(158,40);
 //$pdf->Cell(125,25,"Complaint ID : \n",1);
@@ -107,7 +133,7 @@ $pdf->Image('img/header.jpg',34,10,80,20,'JPG');
 $pdf->Image('img/header.jpg',182,10,80,20,'JPG');
 $pdf->SetXY(20,40);
 //$pdf->Cell(125,25,"Complaint ID : \n",0);
-$pdf->Multicell(120,3,"\nComplaint ID: ".$data."                                             Date: 18-06-2020\n",0,'L'); 
+$pdf->Multicell(120,3,"\nComplaint ID:                                              Date: 18-06-2020\n",0,'L'); 
 $pdf->SetXY(20,46);
 $pdf->Multicell(120,3,"\nAMC From: 18-06-2020                   AMC TO: 18-06-2020\n",0,'L'); 
 $pdf->SetXY(20,52);
